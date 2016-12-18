367f2a4d2f301fa24b7ab518429425ff45b4929d
frandi <dev@rumahfrandi.com>
2016-12-18 13:47:29 +0700

    Add Student.Data

---
+++ student/src/Student.Data/StudentDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student.Data
+{
+    public class StudentDbContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
