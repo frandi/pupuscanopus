c4875857b19800af09ccc70a22d88bfa1987f631
frandi <dev@rumahfrandi.com>
2016-12-20 06:23:16 +0700

    Update Student.Data

--- student/src/Student.Data/BaseEntity.cs
+++ student/src/Student.Data/BaseEntity.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Student.Data
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
-        [Key]
         public Guid Id { get; set; }
         public DateTime Created { get; set; }
         public DateTime? Updated { get; set; }
--- student/src/Student.Data/Student.cs
+++ student/src/Student.Data/Student.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +7,9 @@ namespace Student.Data
 {
     public class Student: BaseEntity
     {
-        [Required]
         public string Code { get; set; }
-        [Required]
         public string FirstName { get; set; }
-        [Required]
         public string LastName { get; set; }
-        [Required]
         public DateTime DateOfBirth { get; set; }
     }
 }
