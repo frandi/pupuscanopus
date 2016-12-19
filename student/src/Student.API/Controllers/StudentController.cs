96c0e94533e6aa52f6556521fb2596a1007272c7
frandi <dev@rumahfrandi.com>
2016-12-20 06:26:58 +0700

    Add Student.API

---
+++ student/src/Student.API/Controllers/StudentController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Student.Service;
+
+namespace Student.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class StudentController : Controller
+    {
+        private IStudentService _studentSvc;
+
+        public StudentController(IStudentService studentSvc)
+        {
+            _studentSvc = studentSvc;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Data.Student> Get()
+        {
+            return _studentSvc.GetStudents();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Data.Student Get(Guid id)
+        {
+            return _studentSvc.GetStudent(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Data.Student value)
+        {
+            _studentSvc.SaveStudent(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(Guid id, [FromBody]Data.Student value)
+        {
+            if (id != value.Id)
+                BadRequest();
+
+            _studentSvc.SaveStudent(value);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+            _studentSvc.DeleteStudent(id);
+        }
+    }
+}
---
+++ student/src/Student.API/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Student.Data.Database;
+using Student.Service;
+using Student.Repository;
+
+namespace Student.API
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<StudentDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("StudentConnection"))
+            );
+
+            services.AddDistributedRedisCache(options =>
+            {
+                options.Configuration = Configuration.GetConnectionString("RedisConnection");
+                options.InstanceName = "master";
+            });
+
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddTransient<IStudentRepository, StudentRepository>();
+            services.AddTransient<IStudentService, StudentService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
