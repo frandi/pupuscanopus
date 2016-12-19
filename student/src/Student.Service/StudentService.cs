da594abb94e0a5c33bfe103be263fe6773bda223
frandi <dev@rumahfrandi.com>
2016-12-20 06:25:23 +0700

    Add Student.Service

---
+++ student/src/Student.Service/StudentService.cs
@@ -0,0 +1,42 @@
+﻿using Student.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Student.Data;
+
+namespace Student.Service
+{
+    public class StudentService: IStudentService
+    {
+        private IStudentRepository _studentRepo;
+
+        public StudentService(IStudentRepository studentRepo)
+        {
+            _studentRepo = studentRepo;
+        }
+
+        public Data.Student GetStudent(Guid id)
+        {
+            return _studentRepo.GetById(id);
+        }
+
+        public IEnumerable<Data.Student> GetStudents()
+        {
+            return _studentRepo.GetAll();
+        }
+
+        public void DeleteStudent(Guid id)
+        {
+            _studentRepo.Delete(id);
+        }
+
+        public void SaveStudent(Data.Student student)
+        {
+            if (_studentRepo.Exists(student.Id))
+                _studentRepo.Update(student);
+            else
+                _studentRepo.Add(student);
+        }
+    }
+}
